courage-browser/courage-core

Adds response class

components/brave_rewards/browser/test/rewards_browsertest_response.h
@@ -1,0 +1,67 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_REWARDS_BROWSER_TEST_REWARDS_BROWSERTEST_RESPONSE_H_
+#define BRAVE_COMPONENTS_BRAVE_REWARDS_BROWSER_TEST_REWARDS_BROWSERTEST_RESPONSE_H_
+
+#include <string>
+
+#include "bat/ledger/mojom_structs.h"
+
+namespace rewards_browsertest {
+
+class RewardsBrowserTestResponse {
+ public:
+  RewardsBrowserTestResponse();
+  ~RewardsBrowserTestResponse();
+
+  void LoadMocks();
+
+  void Get(
+      const std::string& url,
+      int32_t method,
+      int* response_status_code,
+      std::string* response);
+
+  bool WasRequestMade();
+
+  void SetSKUOrder(ledger::SKUOrderPtr order);
+
+  void SetPromotionEmptyKey(const bool empty);
+
+  void SetAlternativePublisherList(const bool alternative);
+
+  void SetUpholdAddress(const std::string& address);
+
+  void SetVerifiedWallet(const bool verified);
+
+  void SetExternalBalance(const std::string& balance);
+
+ private:
+  std::string registrar_vk_;
+  std::string verification_;
+  std::string promotions_;
+  std::string promotion_empty_key_;
+  std::string promotion_claim_;
+  std::string creds_tokens_;
+  std::string creds_tokens_prod_;
+  std::string captcha_;
+  std::string balance_;
+  std::string parameters_;
+  std::string uphold_auth_resp_;
+  std::string uphold_transactions_resp_;
+  std::string uphold_commit_resp_;
+
+  bool request_made_ = false;
+  bool empty_promotion_key_ = false;
+  bool alternative_publisher_list_ = false;
+  ledger::SKUOrderPtr order_;
+  std::string external_wallet_address_;
+  bool verified_wallet_ = false;
+  std::string external_balance_;
+};
+
+}  // namespace rewards_browsertest
+#endif  // BRAVE_COMPONENTS_BRAVE_REWARDS_BROWSER_TEST_REWARDS_BROWSERTEST_RESPONSE_H_

MrWaste/brave-core

components/brave_rewards/browser/test/common/rewards_browsertest_response.h
@@ -1,0 +1,67 @@
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_COMPONENTS_BRAVE_REWARDS_BROWSER_TEST_COMMON_REWARDS_BROWSERTEST_RESPONSE_H_
+#define BRAVE_COMPONENTS_BRAVE_REWARDS_BROWSER_TEST_COMMON_REWARDS_BROWSERTEST_RESPONSE_H_
+
+#include <string>
+
+#include "bat/ledger/mojom_structs.h"
+
+namespace rewards_browsertest {
+
+class RewardsBrowserTestResponse {
+ public:
+  RewardsBrowserTestResponse();
+  ~RewardsBrowserTestResponse();
+
+  void LoadMocks();
+
+  void Get(
+      const std::string& url,
+      int32_t method,
+      int* response_status_code,
+      std::string* response);
+
+  bool WasRequestMade();
+
+  void SetSKUOrder(ledger::SKUOrderPtr order);
+
+  void SetPromotionEmptyKey(const bool empty);
+
+  void SetAlternativePublisherList(const bool alternative);
+
+  void SetUpholdAddress(const std::string& address);
+
+  void SetVerifiedWallet(const bool verified);
+
+  void SetExternalBalance(const std::string& balance);
+
+ private:
+  std::string registrar_vk_;
+  std::string verification_;
+  std::string promotions_;
+  std::string promotion_empty_key_;
+  std::string promotion_claim_;
+  std::string creds_tokens_;
+  std::string creds_tokens_prod_;
+  std::string captcha_;
+  std::string balance_;
+  std::string parameters_;
+  std::string uphold_auth_resp_;
+  std::string uphold_transactions_resp_;
+  std::string uphold_commit_resp_;
+
+  bool request_made_ = false;
+  bool empty_promotion_key_ = false;
+  bool alternative_publisher_list_ = false;
+  ledger::SKUOrderPtr order_;
+  std::string external_wallet_address_;
+  bool verified_wallet_ = false;
+  std::string external_balance_;
+};
+
+}  // namespace rewards_browsertest
+#endif  // BRAVE_COMPONENTS_BRAVE_REWARDS_BROWSER_TEST_COMMON_REWARDS_BROWSERTEST_RESPONSE_H_